280b5d0dd09b2d3781f1b9b75d8d9055e9836895
Marc Creutzberg <mail@mcreutzberg.dk>
2016-11-29 14:49:41 +0100

    Init website

---
+++ BoardGameSleeve/gulpfile.js
@@ -0,0 +1,29 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('sass', function () {
+    return gulp.src('./Sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('../BoardGameSleeveWebsite/css'));
+});
+
+gulp.task('sass:build', function () {
+    return gulp.src('./Sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(cleanCSS({
+          //removing comments /!* ... */
+          keepSpecialComments: 0
+      }))
+      .pipe(gulp.dest('../BoardGameSleeveWebsite/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./Sass/**/*.scss', ['sass']);
+});
+
