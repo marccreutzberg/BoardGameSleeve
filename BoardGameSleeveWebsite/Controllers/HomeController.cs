a5d75f65050b88ba8814312b2241997eee7707e3
efe <eydun95@gmail.com>
2016-12-01 10:56:15 +0100

    DataProjekt tilføjet og services

--- BoardGameSleeveWebsite/Controllers/HomeController.cs
+++ BoardGameSleeveWebsite/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿using BoardGameSleeveWebsite.services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BoardGameSleeveWebsite.Models;
 
 namespace BoardGameSleeveWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        private IService iService;
+
+        public HomeController(IService iService)
+        {
+            this.iService = iService;
+        }
+
         // GET: Home
         public ActionResult Index()
         {
+            //Home model = iService.HomeModel();
+
             return View();
         }
 
---
+++ DataBoardGameSleeve/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBoardGameSleeve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
